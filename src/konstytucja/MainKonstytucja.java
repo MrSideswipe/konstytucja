46b46897efe804af668311832828e4076bc056c4
student23 <student23@pcoip.ki.agh.edu.pl>
2016-12-14 19:08:44 +0100

    sprawdzenie

--- src/konstytucja/MainKonstytucja.java
+++ src/konstytucja/MainKonstytucja.java
@@ -14,12 +14,6 @@ public class MainKonstytucja {
 			if(count == 0)
 				return;
 			View konstytucja = new View(args[0]);
-			//Boolean flag;
-			
-			//Car car1 = new Car();
-			
-			//konstytucja.only_chapter("I");
-			//konstytucja.chapter_art_art("I", "2", "2");
 			
 			switch (count)
 			{
@@ -32,7 +26,8 @@ public class MainKonstytucja {
 					konstytucja.WriteChapter(args[1]);
 				else
 				{
-					// czy tak mozna??????????????????????????
+					number1=Integer.parseInt(args[1]);
+					/*
 					try
 					{
 						number1=Integer.parseInt(args[1]);
@@ -41,6 +36,7 @@ public class MainKonstytucja {
 						out.println("Arguments are invalid");
 			        	return;
 					}
+					*/
 					//
 					if(number1<=243 && number1>=1)
 						konstytucja.WriteArticles(args[1], args[1]);
@@ -62,53 +58,6 @@ public class MainKonstytucja {
 			default:
 				throw new IllegalArgumentException("Arguments are invalid");
 			}
-			
-			
-			/*
-			if(count == 1)
-			{
-				konstytucja.WriteWhole();
-			}
-			//number1=Integer.parseInt(args[1]);
-			//System.out.println(number1);
-			if(count == 2)
-			{
-				if(new ChapterID().belong(args[1]))
-					konstytucja.WriteChapter(args[1]);
-				else
-				{
-					// czy tak mozna??????????????????????????
-					try
-					{
-						number1=Integer.parseInt(args[1]);
-					}
-					catch(NumberFormatException e){
-						out.println("Arguments are invalid");
-			        	return;
-					}
-					//
-					if(number1<=243 && number1>=1)
-						konstytucja.WriteArticles(args[1], args[1]);
-					else
-						throw new IllegalArgumentException("Arguments are invalid");
-				}
-				
-			}
-			
-			if(count == 3)
-			{
-				number1=Integer.parseInt(args[1]);
-				number2=Integer.parseInt(args[2]);
-				if(number2<=243 && number1>=1)
-					konstytucja.WriteArticles(args[1], args[2]);
-				else
-					throw new IllegalArgumentException("Arguments are invalid");
-			}
-			
-			if(count > 3)
-				throw new IllegalArgumentException("Arguments are invalid");
-			
-			*/
 			
 			
 			return;
